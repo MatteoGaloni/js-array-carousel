bfc713374398fe77f741935f141a539db6134665
MatteoGaloni <mtgaloni@gmail.com>
2023-04-19 20:11:36 +0200

    add if to set D-none

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-// const images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"];
+const images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"];
 // console.log(images);
 
-let sliderElement = document.getElementById("slider")
-console.log(sliderElement);
-let image = document.createElement("img");
-image.src = ["./img/01.webp"];
+for (let c = 0; c < images.length; c++) {
+    const sorgenteImg = images[c];
+    // console.log(sorgenteImg)
+    
+    let sliderElement = document.getElementById("slider")
+    // console.log(sliderElement);
+    let image = document.createElement("img");
+    image.src = sorgenteImg;
+    console.log(sorgenteImg);
+    sliderElement.append(image);
 
-image.className = "slide";
-sliderElement.append(image);
+    if (c == 0) {
+        image.className = "slide"
+    } else {
+        image.className = "dNone";
+       
+    }
+
+    
+}
+ 
+
+    
+
+
+
