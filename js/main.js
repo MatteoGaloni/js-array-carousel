69004a4351ca2ac9dcdfc4ff3f0a5fb459915bba
MatteoGaloni <mtgaloni@gmail.com>
2023-04-20 22:34:07 +0200

    add second solution to Prev/Next buttons

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+// *****ADD VARIABLES*****
+// ***********************
 const images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"];
 // console.log(images);
 let currentImage = 0
 // console.log(currentImage)
 
 
-
+// *****START CYCLE TO CREATE IMG ELEMENTS*****
+// ***********************
 for (let c = 0; c < images.length; c++) {
     const sorgenteImg = images[c];
     // console.log(sorgenteImg)
@@ -15,52 +18,73 @@ for (let c = 0; c < images.length; c++) {
     image.src = sorgenteImg;
     // console.log(sorgenteImg);
     sliderElement.append(image);
+    
+// *****CREATE CONDITION IN ORDER TO PROVIDE DBLOCK CLASS*****
+// ***********************
 
     if (c == 0) {
         image.classList = "dBlock"
-    }
-    // else {
-    //     image.classList = "dNone";
-       
-    // }
+    }   
 }
 
+// *****CREATE IMG VARIABLE*****
+// ***********************
 const slideImages = document.querySelectorAll("#slider > img")
 // console.log(slideImages)
 
+// *****ADD EVENTLISTENER ON CLICK (1ST TRICKY SOLUTION)*****
+// ***********************
+// const btnNext = document.getElementById("btnNext")
+// btnNext.addEventListener("click", function () {
+
+//     if (currentImage == 4) {
+//         slideImages[4].classList.remove("dBlock")
+//         currentImage = -1
+//     }
+
+//     const ImgElement = slideImages[currentImage+1]
+//     console.log(ImgElement)
+//     ImgElement.classList.add("dBlock")
+//     const ImgElement1 = slideImages[currentImage]
+//     if (currentImage > -1) {
+//         ImgElement1.classList.remove("dBlock")
+//     }
+    
+//     currentImage ++
+//     console.log(currentImage)
+    
+// })
+
+// *****ADD EVENTLISTENER(2nd SOLUTION)*****
+// ***********************
+
 const btnNext = document.getElementById("btnNext")
 btnNext.addEventListener("click", function () {
-    // console.log(btnNext)
-    // currentImage = [slideImages]
-    // console.log(currentImage)
-    if (currentImage == 4) {
-        slideImages[4].classList.remove("dBlock")
-        currentImage = -1
+
+    slideImages[currentImage].classList.remove("dBlock")
+    currentImage++
+    if (currentImage == slideImages.length) {
+        currentImage = 0
+        console.log(currentImage)        
     }
-
-    const ImgElement = slideImages[currentImage+1]
-    console.log(ImgElement)
-    ImgElement.classList.add("dBlock")
-    const ImgElement1 = slideImages[currentImage]
-    if (currentImage > -1) {
-        ImgElement1.classList.remove("dBlock")        
-    }
-
-    
-    
-
-
-
-        // currentImage.classList = "dBlock"
-        // console.log(currentImage)
-    
-   
-
-
-    currentImage ++
-    console.log(currentImage)
-    
+    slideImages[currentImage].classList.add("dBlock")  
 })
+
+const btnPrevious = document.getElementById("btnPrevious")
+btnPrevious.addEventListener("click", function () {
+
+    slideImages[currentImage].classList.remove("dBlock")
+       
+    if (currentImage == 0) {
+        currentImage = slideImages.length              
+    }      
+    currentImage--
+    slideImages[currentImage].classList.add("dBlock")
+
+})
+
+
+
 
 
  
